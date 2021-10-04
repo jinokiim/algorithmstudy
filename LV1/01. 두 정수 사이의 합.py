ca2b729d1575c3b93f1c0dae2d62eaaf5a77b756
Jinho <88222461+jinokiim@users.noreply.github.com>
2021-10-04 20:05:15 +0900

    Update and rename 01. 두 정수 사이의 합.md to 01. 두 정수 사이의 합.py

---
+++ LV1/01. 두 정수 사이의 합.py
@@ -0,0 +1,31 @@
+
+  
+### 1회
+```python
+def solution(a, b):
+    answer = 0
+    if a <= b:
+        answer = sum(range(a, b+1))
+    else:
+        answer = sum(range(b, a+1))
+    return answer
+```
+
+### 2회
+```python
+def solution(a, b):
+    if a>b:
+        a, b = b, a
+        return sum(range(a, b+1))
+    else:
+        return sum(range(a, b+1))
+```
+
+
+### 다른 사람의 풀이
+
+```python
+def adder(a, b):
+    return (abs(a-b)+1)*(a+b)//2
+print(adder(a, b))
+```
